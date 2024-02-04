8dc9a2221f32bd1c45458392d8e27758f2b738b2
salmanck66 <salmanulfaris.c.k10@gmail.com>
2024-02-05 01:13:18 +0530

    dsad

--- controllers/admin.js
+++ controllers/admin.js
@@ -19,5 +19,33 @@ let productm = (req, res) => {
     console.log("admin product management");
     res.render('admin/productm',{layout:"adminLayout.hbs"})
   }
+let addproduct = (req, res) => {
+    console.log("admin product management");
+    res.render('admin/addproduct',{layout:"adminLayout.hbs"})
+  }
+let coupon = (req, res) => {
+    console.log("admin coupn management");
+    res.render('admin/coupons',{layout:"adminLayout.hbs"})
+  }
+let categories = (req, res) => {
+    console.log("admin categories management");
+    res.render('admin/category',{layout:"adminLayout.hbs"})
+  }
+let banner = (req, res) => {
+    console.log("admin banner management");
+    res.render('admin/banner',{layout:"adminLayout.hbs"})
+  }
+let payments = (req, res) => {
+    console.log("admin banner management");
+    res.render('admin/payments',{layout:"adminLayout.hbs"})
+  }
+let settings = (req, res) => {
+    console.log("admin banner management");
+    res.render('admin/settings',{layout:"adminLayout.hbs"})
+  }
+let profile = (req, res) => {
+    console.log("admin banner management");
+    res.render('admin/profile',{layout:"adminLayout.hbs"})
+  }
 
-  module.exports = {homePage,logIn,Forget,order,productm}
+  module.exports = {homePage,logIn,Forget,order,productm,addproduct,coupon,categories,banner,payments,settings,profile}
--- routes/admin.js
+++ routes/admin.js
@@ -7,5 +7,12 @@ router.get('/forget',adminControllers.Forget)
 router.get('/dashboard',adminControllers.homePage)
 router.get('/orders',adminControllers.order)
 router.get('/productm',adminControllers.productm)
+router.get('/addproduct',adminControllers.addproduct)
+router.get('/coupon',adminControllers.coupon)
+router.get('/category',adminControllers.categories)
+router.get('/banner',adminControllers.banner)
+router.get('/payments',adminControllers.payments)
+router.get('/settings',adminControllers.settings)
+router.get('/profile',adminControllers.profile)
 
 module.exports=router;
