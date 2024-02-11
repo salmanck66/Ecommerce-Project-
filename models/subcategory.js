47b37c3352458e644d6a523ece2e890d17407cd3
salmanck66 <salmanulfaris.c.k10@gmail.com>
2024-02-11 23:09:02 +0530

    df

--- models/subcategory.js
+++ models/subcategory.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const SubCategorySchema = new mongoose.Schema({
-    subCategoryName:{type:String,required:true},
-    parentCategory:{type:String,required:true}
-},{versionKey:false});
+    subCategoryName: { type: String, required: true },
+    parentCategory: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true }
+}, { versionKey: false });
 
 const SubCategory=mongoose.model('SubCategory',SubCategorySchema,'SubCategory');
 module.exports=SubCategory;
