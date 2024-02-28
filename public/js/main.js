4cb195b132fd4b0a01c0c663828f412111243215
salmanck66 <salmanulfaris.c.k10@gmail.com>
2024-02-28 18:34:10 +0530

    df

--- public/js/main.js
+++ public/js/main.js
@@ -215,13 +215,18 @@
     /*==================================================================
     [ +/- num product ]*/
     $('.btn-num-product-down').on('click', function(){
-        var numProduct = Number($(this).next().val());
-        if(numProduct > 0) $(this).next().val(numProduct - 1);
+        var inputField = $(this).next('.num-product');
+        var numProduct = Number(inputField.val());
+        var numProduct = numProduct
+        if(numProduct > 1) {
+            inputField.val(numProduct - 1);
+        }
     });
-
+    
     $('.btn-num-product-up').on('click', function(){
-        var numProduct = Number($(this).prev().val());
-        $(this).prev().val(numProduct + 1);
+        var inputField = $(this).prev('.num-product');
+        var numProduct = Number(inputField.val());
+        inputField.val(numProduct + 1);
     });
 
     /*==================================================================
